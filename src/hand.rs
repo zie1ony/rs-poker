75b7667b0957b2c75c2b36dfa69b68301f23ffab
Elliott Clark <elliott@fb.com>
2016-09-26 09:20:13 -0700

    Add on Hand.push()

--- src/hand.rs
+++ src/hand.rs
@@ -1,4 +1,4 @@
-use card::Card;
+use card::*;
 
 pub struct Hand {
     cards: Vec<Card>,
@@ -6,16 +6,33 @@ pub struct Hand {
 
 impl Hand {
     pub fn new() -> Hand {
-        Hand { cards: vec![] }
+        Hand { cards: Vec::with_capacity(5) }
+    }
+    /// Add card at to the hand.
+    /// No verification is done at all.
+    pub fn push(&mut self, c: Card) {
+        self.cards.push(c);
+    }
+    pub fn len(&self) -> usize {
+        self.cards.len()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use card::*;
 
     #[test]
     fn test_add_card() {
         assert!(true);
+        let mut h = Hand::new();
+        let c = Card {
+            value: Value::Three,
+            suit: Suit::Spade,
+        };
+        h.push(c);
+        // Make sure that the card was added to the vec.
+        assert_eq!(1, h.len());
     }
 }
