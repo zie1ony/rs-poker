f26eefcc5e2714e56611ff77485848c113d26866
Elliott Clark <elliott@fb.com>
2016-10-31 17:26:02 -0700

    Add on tie breaking ranks.

--- src/rank.rs
+++ src/rank.rs
@@ -4,15 +4,15 @@ use vec_map::VecMap;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum Rank {
-    HighCard,
-    OnePair,
-    TwoPair,
-    ThreeOfAKind,
-    Straight,
-    Flush,
-    FullHouse,
-    FourOfAKind,
-    StraightFlush,
+    HighCard(usize),
+    OnePair(usize),
+    TwoPair(usize),
+    ThreeOfAKind(usize),
+    Straight(usize),
+    Flush(usize),
+    FullHouse(usize),
+    FourOfAKind(usize),
+    StraightFlush(usize),
 }
 
 // Big ugly constant for all the straghts.
@@ -83,24 +83,21 @@ impl Rankable for Hand {
             // It will be used to differentiate strenght of the same rank
             potential_hand_rank |= 1 << v;
             // Keep track of counts for each card.
-            if value_to_count.contains_key(v) {
-                value_to_count[v] += 1;
-            } else {
-                value_to_count.insert(v, 1);
-            }
+            let e = value_to_count.entry(v).or_insert(0);
+            *e += 1;
         }
 
         // Now rotate the value to count map.
         for (value, count) in value_to_count {
-            if count_to_value.contains_key(count) {
-                count_to_value[count].push(Value::from_usize(value));
-            } else {
-                count_to_value.insert(count, vec![Value::from_usize(value)]);
-            }
+            // Get the entry for the map, or insert it into the map.
+            let e = count_to_value.entry(count).or_insert_with(|| vec![]);
+            // Now that it has to be there, push the new value.
+            (*e).push(Value::from_usize(value));
         }
 
+        // The major deciding factor for hand rank
+        // is the number of unique card values.
         let unique_card_count = value_set.count_ones();
-
 
         // Now that we should have all the information needed.
         // Lets do this.
@@ -111,29 +108,50 @@ impl Rankable for Hand {
             let suit_count = suit_set.count_ones();
             let is_flush = suit_count == 1;
             match (self.rank_straight(potential_hand_rank), is_flush) {
-                (Some(_), true) => Rank::StraightFlush,
-                (Some(_), false) => Rank::Straight,
-                (None, true) => Rank::Flush,
-                (None, false) => Rank::HighCard,
+                (Some(rank), true) => Rank::StraightFlush(rank),
+                (Some(rank), false) => Rank::Straight(rank),
+                (None, true) => Rank::Flush(potential_hand_rank),
+                (None, false) => Rank::HighCard(potential_hand_rank),
             }
         } else if unique_card_count == 2 {
             // This can either be full house, or four of a kind.
-            if count_to_value.contains_key(3) {
-                Rank::FullHouse
-            } else {
-                Rank::FourOfAKind
+            match count_to_value.get(3) {
+                Some(three_value) => {
+                    let major_rank = 1 << three_value[0].clone() as usize;
+                    // Remove the card that we have three of from the minor rank.
+                    let minor_rank = potential_hand_rank ^ major_rank;
+                    // then join the two ranks
+                    Rank::FullHouse(major_rank << 13 | minor_rank)
+                }
+                None => {
+                    let major_rank = 1 << (count_to_value[4][0].clone() as usize);
+                    let minor_rank = potential_hand_rank ^ major_rank;
+                    Rank::FourOfAKind(major_rank << 13 | minor_rank)
+                }
             }
         } else if unique_card_count == 3 {
             // this can be three of a kind or two pair.
-            if count_to_value.contains_key(3) {
-                Rank::ThreeOfAKind
-            } else {
-                Rank::TwoPair
+            match count_to_value.get(3) {
+                Some(three_value) => {
+                    let major_rank = 1 << (three_value[0].clone() as usize);
+                    let minor_rank = potential_hand_rank ^ major_rank;
+                    Rank::ThreeOfAKind(major_rank << 13 | minor_rank)
+                }
+                None => {
+                    // get the values of the pairs
+                    let pairs = &count_to_value[2];
+                    let major_rank = 1 << pairs[0].clone() as usize |
+                                     1 << pairs[1].clone() as usize;
+                    let minor_rank = potential_hand_rank ^ major_rank;
+                    Rank::TwoPair(major_rank << 13 | minor_rank)
+                }
             }
         } else {
             // this is unique_card_count == 4
             assert!(unique_card_count == 4);
-            Rank::OnePair
+            let major_rank = 1 << count_to_value[2][0].clone() as usize;
+            let minor_rank = potential_hand_rank ^ major_rank;
+            Rank::OnePair(major_rank << 13 | minor_rank)
         }
     }
 }
@@ -148,9 +166,14 @@ mod tests {
 
     #[test]
     fn test_cmp() {
-        assert!(Rank::HighCard < Rank::StraightFlush);
-        assert!(Rank::HighCard < Rank::FourOfAKind);
-        assert!(Rank::HighCard < Rank::ThreeOfAKind);
+        assert!(Rank::HighCard(0) < Rank::StraightFlush(0));
+        assert!(Rank::HighCard(0) < Rank::FourOfAKind(0));
+        assert!(Rank::HighCard(0) < Rank::ThreeOfAKind(0));
+    }
+
+    #[test]
+    fn test_cmp_high() {
+        assert!(Rank::HighCard(0) < Rank::HighCard(100));
     }
 
     #[test]
@@ -163,7 +186,11 @@ mod tests {
                                              Card{value: Value::Five, suit: Suit::Club},
                                              ]);
 
-        assert!(Rank::HighCard == hand.rank());
+        let rank = 1 << Value::Ace as usize | 1 << Value::Eight as usize |
+                   1 << Value::Nine as usize | 1 << Value::Ten as usize |
+                   1 << Value::Five as usize;
+
+        assert!(Rank::HighCard(rank) == hand.rank());
     }
 
     #[test]
@@ -176,7 +203,11 @@ mod tests {
                                              Card{value: Value::Five, suit: Suit::Diamond},
                                              ]);
 
-        assert!(Rank::Flush == hand.rank());
+        let rank = 1 << Value::Ace as usize | 1 << Value::Eight as usize |
+                   1 << Value::Nine as usize | 1 << Value::Ten as usize |
+                   1 << Value::Five as usize;
+
+        assert!(Rank::Flush(rank) == hand.rank());
     }
 
     #[test]
@@ -189,11 +220,13 @@ mod tests {
                                              Card{value: Value::Nine, suit: Suit::Spade},
                                              ]);
 
-        assert!(Rank::FullHouse == hand.rank());
+        let rank = (1 << (Value::Nine as usize)) << 13 | 1 << (Value::Ace as usize);
+        assert!(Rank::FullHouse(rank) == hand.rank());
     }
 
     #[test]
     fn test_two_pair() {
+        // Make a two pair hand.
         let hand = Hand::new_with_cards(vec![
                                              Card{value: Value::Ace, suit: Suit::Diamond},
                                              Card{value: Value::Ace, suit: Suit::Club},
@@ -202,7 +235,9 @@ mod tests {
                                              Card{value: Value::Ten, suit: Suit::Spade},
                                              ]);
 
-        assert!(Rank::TwoPair == hand.rank());
+        let rank = (1 << Value::Ace as usize | 1 << Value::Nine as usize) << 13 |
+                   1 << Value::Ten as usize;
+        assert!(Rank::TwoPair(rank) == hand.rank());
     }
 
     #[test]
@@ -215,7 +250,10 @@ mod tests {
                                              Card{value: Value::Ten, suit: Suit::Spade},
                                              ]);
 
-        assert!(Rank::OnePair == hand.rank());
+        let rank = (1 << Value::Ace as usize) << 13 | 1 << Value::Nine as usize |
+                   1 << Value::Eight as usize | 1 << Value::Ten as usize;
+
+        assert!(Rank::OnePair(rank) == hand.rank());
     }
 
     #[test]
@@ -228,7 +266,8 @@ mod tests {
                                              Card{value: Value::Ten, suit: Suit::Spade},
                                              ]);
 
-        assert!(Rank::FourOfAKind == hand.rank());
+        assert!(Rank::FourOfAKind((1 << (Value::Ace as usize) << 13) |
+                                  1 << (Value::Ten as usize)) == hand.rank());
     }
 
     #[test]
@@ -241,7 +280,7 @@ mod tests {
                                              Card{value: Value::Five, suit: Suit::Spade},
                                              ]);
 
-        assert!(Rank::Straight == hand.rank());
+        assert!(Rank::Straight(0) == hand.rank());
     }
 
 
@@ -255,7 +294,7 @@ mod tests {
                                              Card{value: Value::Six, suit: Suit::Diamond},
                                              ]);
 
-        assert!(Rank::Straight == hand.rank());
+        assert!(Rank::Straight(1) == hand.rank());
     }
 
     #[test]
@@ -268,7 +307,11 @@ mod tests {
                                              Card{value: Value::Six, suit: Suit::Diamond},
                                              ]);
 
-        assert!(Rank::ThreeOfAKind == hand.rank());
+
+        let rank = (1 << (Value::Two as usize)) << 13 | 1 << (Value::Five as usize) |
+                   1 << (Value::Six as usize);
+
+        assert!(Rank::ThreeOfAKind(rank) == hand.rank());
     }
 
 
