7d916ebac73c800acf774dcfe7f50eef9007d638
Elliott <elliott.neil.clark@gmail.com>
2017-04-10 10:41:13 -0700

    Add on fuzz script to validate rank_seven

---
+++ fuzz/fuzzers/rank_seven.rs
@@ -0,0 +1,22 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate rs_poker;
+use std::str;
+use rs_poker::core::{Hand, Rankable, CardIter};
+
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = str::from_utf8(data) {
+        if let Ok(h) = Hand::new_from_str(s) {
+            if h.len() == 7 {
+                let r_seven = h.rank_seven();
+                let r_five_max = CardIter::new(&h[..], 5)
+                    .map(|cv| cv.rank())
+                    .max()
+                    .unwrap();
+                assert_eq!(r_five_max, r_seven);
+            }
+        }
+    }
+});
