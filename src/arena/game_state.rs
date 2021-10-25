5ff04c0e21f953b5209cf197406312078db606ff
Elliott Clark <elliott.neil.clark@gmail.com>
2021-10-24 23:15:24 -0700

    Preflop with blinds

--- src/arena/game_state.rs
+++ src/arena/game_state.rs
@@ -1,51 +1,154 @@
 use fixedbitset::FixedBitSet;
 
-use crate::core::{Card, FlatDeck, Hand};
+use crate::core::{Card, Hand};
 pub enum Round {
     Starting,
     Preflop,
     Flop,
     Turn,
     River,
+    Showdown,
 }
 
 pub struct RoundData {
-    round_bet: Vec<usize>,
-    betting_complete: FixedBitSet,
-    min_bet: usize,
+    pub player_active: FixedBitSet,
+    // The minimum allowed bet.
+    pub min_bet: usize,
+    // The value to be called.
+    pub bet: usize,
+    // How much each player has put in so far.
+    pub player_bet: Vec<usize>,
+    // The number of times the player has put in money.
+    pub bet_count: Vec<usize>,
+    // The index of the next player to act.
+    pub to_act_idx: usize,
+}
+
+impl RoundData {
+    pub fn advance(&mut self) {
+        loop {
+            self.to_act_idx = (self.to_act_idx + 1) % self.player_active.len();
+            if self.player_active.count_ones(..) == 0
+                || self.player_active.contains(self.to_act_idx)
+            {
+                break;
+            }
+        }
+    }
 }
 
 pub struct GameState {
-    pub initial_num_players: usize,
-    pub stacks: Vec<usize>,
-    pub hands: Vec<Hand>,
+    pub num_players: usize,
     pub player_active: FixedBitSet,
+    pub stacks: Vec<usize>,
+    pub big_blind: usize,
+    pub small_blind: usize,
+    pub hands: Vec<Hand>,
     pub dealer_idx: usize,
+    pub last_raise: usize,
     pub round: Round,
+    pub round_data: Vec<RoundData>,
     pub board: Vec<Card>,
 }
 
 impl GameState {
-    pub fn new(stacks: Vec<usize>, _big_blind: usize, _small_blind: usize) -> Self {
-        let num = stacks.len();
-        let mut active_mask = FixedBitSet::with_capacity(num);
+    pub fn new(stacks: Vec<usize>, big_blind: usize, small_blind: usize) -> Self {
+        let num_players = stacks.len();
+
+        // Everyone's active right now
+        let mut active_mask = FixedBitSet::with_capacity(num_players);
         active_mask.set_range(.., true);
         GameState {
-            initial_num_players: num,
+            num_players,
             stacks,
+            big_blind,
+            small_blind,
             player_active: active_mask,
             dealer_idx: 0,
-            hands: vec![Hand::default(); num],
+            last_raise: 0,
+            hands: vec![Hand::default(); num_players],
             round: Round::Starting,
             board: vec![],
+            round_data: vec![],
         }
     }
 
-    pub fn deal_preflop(&mut self, deck: &mut FlatDeck) {
-        for h in &mut self.hands {
-            h.push(deck.deal().unwrap());
-            h.push(deck.deal().unwrap());
+    pub fn advance_round(&mut self) {
+        match self.round {
+            Round::Starting => self.advance_preflop(),
+            Round::Preflop => self.advance_flop(),
+            _ => todo!(),
         }
+    }
+
+    fn advance_preflop(&mut self) {
         self.round = Round::Preflop;
+        self.round_data.push(self.round_data());
+        self.do_bet(self.small_blind, true);
+        self.do_bet(self.big_blind, true);
+    }
+
+    fn advance_flop(&mut self) {
+        self.round = Round::Flop;
+        self.round_data.push(self.round_data())
+    }
+
+    fn round_data(&self) -> RoundData {
+        RoundData {
+            player_bet: vec![0; self.num_players],
+            bet_count: vec![0; self.num_players],
+            bet: 0,
+            min_bet: self.big_blind,
+            player_active: self.player_active.clone(),
+            to_act_idx: self.left_of_dealer(),
+        }
+    }
+
+    pub fn do_bet(&mut self, ammount: usize, is_forced: bool) -> Option<usize> {
+        if let Some(round_idx) = self.round_index() {
+            // Grab the current round data.
+            let rd = &mut self.round_data[round_idx];
+
+            // Which player is next to act
+            let idx = rd.to_act_idx;
+            // Can't bet what's not there
+            let bet_ammount = ammount.min(self.stacks[idx]);
+            // Take the money out.
+            self.stacks[idx] -= bet_ammount;
+            // If they put money into the pot then they are done this turn.
+            rd.player_active.set(idx, is_forced);
+
+            rd.player_bet[idx] += bet_ammount;
+            rd.bet_count[idx] += 1;
+
+            rd.bet = rd.bet.max(bet_ammount);
+
+            // We're out and can't continue.
+            if self.stacks[idx] == 0 {
+                self.player_active.set(idx, false);
+                rd.player_active.set(idx, false);
+            }
+
+            // Advance the next to act.
+            rd.advance();
+
+            Some(bet_ammount)
+        } else {
+            None
+        }
+    }
+
+    pub fn left_of_dealer(&self) -> usize {
+        (self.dealer_idx + 1) % self.num_players
+    }
+
+    fn round_index(&self) -> Option<usize> {
+        match self.round {
+            Round::Preflop => Some(0),
+            Round::Flop => Some(1),
+            Round::Turn => Some(2),
+            Round::River => Some(3),
+            _ => None,
+        }
     }
 }
--- src/arena/simulation.rs
+++ src/arena/simulation.rs
@@ -1,4 +1,5 @@
-use crate::core::{Deck, FlatDeck};
+use crate::arena::game_state::Round;
+use crate::core::{Card, Deck, FlatDeck};
 
 use super::agent::Agent;
 
@@ -12,6 +13,10 @@ pub struct HoldemSimulation {
 
 impl HoldemSimulation {
     pub fn new(game_state: GameState) -> Self {
+        HoldemSimulation::new_with_agents(game_state, vec![])
+    }
+
+    pub fn new_with_agents(game_state: GameState, agents: Vec<Box<dyn Agent>>) -> Self {
         let mut d = Deck::default();
 
         for hand in game_state.hands.iter() {
@@ -20,22 +25,56 @@ impl HoldemSimulation {
             }
         }
 
+        let mut flat_deck: FlatDeck = d.into();
+        flat_deck.shuffle();
+
         Self {
             game_state,
-            deck: d.into(),
-            agents: vec![],
+            agents,
+            deck: flat_deck,
         }
     }
 
     pub fn step(&mut self) {
         match self.game_state.round {
-            super::game_state::Round::Starting => self.start(),
+            Round::Starting => self.start(),
+            Round::Preflop => self.preflop(),
+            Round::Flop => self.flop(),
             _ => todo!(),
         }
     }
 
     fn start(&mut self) {
-        self.game_state.deal_preflop(&mut self.deck);
+        for h in &mut self.game_state.hands {
+            h.push(self.deck.deal().unwrap());
+            h.push(self.deck.deal().unwrap());
+        }
+        self.game_state.advance_round();
+    }
+
+    fn preflop(&mut self) {
+        // self.run_betting_round();
+        self.game_state.advance_round();
+    }
+
+    fn flop(&mut self) {
+        self.deal_comunity(3);
+        // self.run_betting_round();
+        self.game_state.advance_round();
+    }
+
+    fn deal_comunity(&mut self, num_cards: usize) {
+        let mut community_cards: Vec<Card> =
+            (0..num_cards).map(|_| self.deck.deal().unwrap()).collect();
+        // Add all the cards to the hands as well.
+        for h in &mut self.game_state.hands {
+            for c in &community_cards {
+                // push a copy
+                h.push(*c);
+            }
+        }
+        // Drain the community_cards vec into the game_state board.
+        self.game_state.board.append(&mut community_cards);
     }
 }
 
@@ -45,11 +84,15 @@ mod tests {
 
     #[test]
     fn test_single_step_agent() {
-        let game_state = GameState::new(vec![100, 40], 10, 5);
+        let stacks = vec![100; 9];
+        let game_state = GameState::new(stacks, 10, 5);
         let mut sim = HoldemSimulation::new(game_state);
 
+        assert_eq!(100, sim.game_state.stacks[1]);
         // We are starting out.
         sim.step();
-        // Now run preflop
+        // assert that blinds are there
+        assert_eq!(95, sim.game_state.stacks[1]);
+        assert_eq!(90, sim.game_state.stacks[2]);
     }
 }
