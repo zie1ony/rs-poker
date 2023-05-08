fe39f9ee6f58fc5dddca2f519e39afa8b70f88ab
Elliott <elliott.neil.clark@gmail.com>
2023-05-07 23:26:27 -0500

    chore: add documentation for rs_poker::utils::PlayerBitSet (#40)
    - Doctests
    - Documentation
    - Better code docs

--- src/utils/player_bit_set.rs
+++ src/utils/player_bit_set.rs
@@ -1,35 +1,85 @@
 use std::ops::BitOr;
 
+/// A struct representing a bit set for players.
+///
+/// # Examples
+///
+/// ```
+/// use rs_poker::utils::PlayerBitSet;
+/// let mut active_players = PlayerBitSet::new(9);
+///
+/// // Player 4 folds
+/// active_players.disable(4);
+/// assert_eq!(8, active_players.count());
+/// ```
 #[derive(Debug, Default, Clone, Copy)]
 pub struct PlayerBitSet {
     set: u16,
 }
 
 impl PlayerBitSet {
+    /// Creates a new `PlayerBitSet` with `players` number of players.
     pub fn new(players: usize) -> Self {
         let set = (1 << players) - 1;
         Self { set }
     }
+
+    /// Returns the number of players enabled in the bit set.
     pub fn count(&self) -> usize {
         self.set.count_ones() as usize
     }
+
+    /// Returns `true` if the bit set is empty.
     pub fn empty(&self) -> bool {
         self.set == 0
     }
+
+    /// Enables the player at `idx` position in the bit set.
     pub fn enable(&mut self, idx: usize) {
         self.set |= 1 << idx;
     }
+
+    /// Disables the player at `idx` position in the bit set.
     pub fn disable(&mut self, idx: usize) {
         self.set &= !(1 << idx);
     }
+
+    /// Returns `true` if the player at `idx` position in the bit set is enabled.
     pub fn get(&self, idx: usize) -> bool {
         (self.set & (1 << idx)) != 0
     }
+
+    /// Returns an iterator over the active players in the bit set.
     pub fn ones(self) -> ActivePlayerBitSetIter {
         ActivePlayerBitSetIter { set: self.set }
     }
 }
 
+/// Implements the BitOr trait for PlayerBitSet, allowing two PlayerBitSet instances to be combined
+/// using the | operator.
+///
+/// # Examples
+///
+/// ```
+/// use rs_poker::utils::PlayerBitSet;
+///
+/// let mut a = PlayerBitSet::default();
+/// a.enable(0);
+/// a.enable(2);
+///
+/// let mut b = PlayerBitSet::default();
+/// b.enable(1);
+/// b.enable(2);
+///
+/// let c = a | b;
+/// assert_eq!(c.get(0), true);
+/// assert_eq!(c.get(1), true);
+/// assert_eq!(c.get(2), true);
+/// ```
+///
+/// Here, two `PlayerBitSet` instances `a` and `b` are combined using the `|` operator to produce a new
+/// `PlayerBitSet` instance `c`. `c` contains all the bits set in either `a` or `b`.
+///
 impl BitOr for PlayerBitSet {
     type Output = PlayerBitSet;
 
@@ -40,22 +90,46 @@ impl BitOr for PlayerBitSet {
     }
 }
 
+/// An iterator over the active players in a bit set.
 pub struct ActivePlayerBitSetIter {
     set: u16,
 }
 
+/// # Examples
+///
+/// ```
+/// use rs_poker::utils::PlayerBitSet;
+///
+/// let mut set = PlayerBitSet::default();
+///
+/// set.enable(0);
+/// set.enable(4);
+/// set.enable(2);
+///
+/// let mut iter = set.ones();
+///
+/// assert_eq!(iter.next(), Some(0));
+/// assert_eq!(iter.next(), Some(2));
+/// assert_eq!(iter.next(), Some(4));
+/// assert_eq!(iter.next(), None);
+/// ```
+///
+/// Here, we create a `PlayerBitSet` instance `set` and set some bits in it. We then create an `ActivePlayerBitSetIter`
+/// iterator from the `set` instance and use it to iterate over the active players. We verify that the iterator returns
+/// the correct indices of the active players.
 impl Iterator for ActivePlayerBitSetIter {
     type Item = usize;
 
+    /// Returns the next active player in the bit set, or `None` if there are no more.
     fn next(&mut self) -> Option<Self::Item> {
         if self.set == 0 {
             None
         } else {
-            // Find the index of the first non-zero
+            // Find the index of the first non-zero bit
             let idx = self.set.trailing_zeros() as usize;
-            // Then set the first non-zero to zero
+            // Then set the first non-zero bit to zero
             self.set &= !(1 << idx);
-            // Then emit the next one
+            // Then emit the next active player
             Some(idx)
         }
     }
